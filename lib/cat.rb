ccb4fffeba9926abb5c6a2b05ef7b43f9eb84cca
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-20 18:23:21 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,7 +2,7 @@ class Cat
     attr_accessor :name, :owner, :mood
     @@all = []
 
-    def initialize(name, Owner)
+    def initialize(name, @owner)
       #@owner = Owner
       @name = name
       @mood = "nervous"
